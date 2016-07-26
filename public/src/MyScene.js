1ccff4cc946d72bdee3457835f666f816d27943f
Makoto Ishida <makotoishida@gmail.com>
2016-07-26 02:55:49 -1000

    Updated at hyperdev.com

--- public/src/MyScene.js
+++ public/src/MyScene.js
@@ -6,21 +6,24 @@ var MyLayer = cc.Layer.extend({
         this._super();
         var size = cc.director.getWinSize();
 
+        this.sprite = new cc.Sprite(IMAGES.HelloWorld);
+        //this.sprite.setAnchorPoint(0.5, 0.5);
+        this.sprite.setPosition(size.width / 2, size.height / 2);
+        this.sprite.setScale(size.height / this.sprite.getContentSize().height);
+        this.addChild(this.sprite, 0);
+        
+        this.sprite.runAction(
+          new cc.rotateBy(9, 360).repeatForever()
+        );
+        
         this.helloLabel = new cc.LabelTTF("Hello World", "Impact", 38);
         this.helloLabel.setPosition(size.width / 2, size.height / 2);
         this.addChild(this.helloLabel, 5);
+        
         this.helloLabel.runAction(
           new cc.rotateBy(2, 360).repeatForever()
         );
 
-        this.sprite = new cc.Sprite(IMAGES.HelloWorld);
-        this.sprite.setAnchorPoint(0.5, 0.5);
-        this.sprite.setPosition(size.width / 2, size.height / 2);
-        this.sprite.setScale(size.height / this.sprite.getContentSize().height);
-        this.addChild(this.sprite, 0);
-        this.sprite.runAction(
-          new cc.rotateBy(9, 360).repeatForever()
-        );
     }
 });
 
