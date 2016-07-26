caffcc3a48a65da81e001a1fd7c48f3fec34cbb0
Makoto Ishida <makotoishida@gmail.com>
2016-07-26 02:43:40 -1000

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use(express.static('public'));
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Listening on port ' + listener.address().port);
 });
